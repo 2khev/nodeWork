53c8451727918191450896bd0429272958355094
2khev <2khev61@gmail.com>
2017-07-16 22:42:32 -0400

    added multiRequest.js

---
+++ multiRequest.js
@@ -0,0 +1,25 @@
+function placeAnOrder(requestNumber){
+
+    console.log("Costomer Request:", requestNumber);
+
+    cookAndGivenFood(function () {
+        console.log("Given Food Request", requestNumber);
+    });
+}
+
+function cookAndGivenFood(yellBack){
+    setTimeout(yellBack, 4000);
+}
+
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
+placeAnOrder(6);
+placeAnOrder(7);
+placeAnOrder(8);
+placeAnOrder(9);
+placeAnOrder(10);
+
+
